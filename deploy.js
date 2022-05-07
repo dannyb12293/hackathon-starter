62c27fd9fd093334eee516c0851526323064de38
Daniel Burgos <dannyb12293@yahoo.com>
2022-05-07 15:01:40 -0400

    updated deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.207.80.47',
+      host: '34.235.129.218',
       username: 'ubuntu',
       privateKey: 'labsuser.pem'
     })
